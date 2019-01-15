bcc206d85f93b9638b7bd82fdef1be25561beb15
Hoang Vu <contact@hoang.vu>
2019-01-15 16:30:19 +0100

    add graphql hello world and koa to server side

---
+++ src/server/index.ts
@@ -0,0 +1,18 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import apolloServer from './apollo';
+
+const app = new Koa();
+const port = 5000;
+const router = new Router();
+
+apolloServer.applyMiddleware({ app });
+
+router.get('/', async ctx => {
+    ctx.response.body = 'heheh';
+    ctx.response.type = 'text/html';
+});
+console.log(apolloServer.graphqlPath);
+app.use(router.routes())
+    .use(router.allowedMethods())
+    .listen(port);
